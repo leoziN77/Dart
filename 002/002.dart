8f925c84fa65ad553c2849188810149c040330d8
Lucas Cruz <lcs.gomes33@gmail.com>
2022-03-23 00:10:53 -0300

    desenvolvimento de testes

---
+++ 002/002.dart
@@ -0,0 +1,21 @@
+void main() {
+  String nome = 'Lucas';
+  nome += ' Cruz';
+  print('Meu nome é ${nome}.');
+
+  int num = 19;
+  print('Minha idade é ${num} anos.');
+
+  double pct = 99.99;
+  print('Estou me sentindo ${pct}% confiante!');
+
+  bool mlh = true;
+
+  if (pct < 100) {
+    print('Não estou confiante o suficiente!');
+  }
+
+  if (mlh = true) {
+    print('Pretendo melhorar!');
+  }
+}
